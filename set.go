1222f9eee7d45c004ace360cdaa5a9a8cbf30269
XuanLee-HEALER <mouse.tree.lee@gmail.com>
2023-12-29 16:22:58 +0800

    set和线程安全set

--- set.go
+++ set.go
@@ -3,15 +3,75 @@ package godskeqing
 import (
 	"fmt"
 	"strings"
+	"sync"
 )
 
-type Set[T comparable] map[T]struct{}
+type Set[K comparable] map[K]struct{}
 
-func NewSet[T comparable]() Set[T] {
-	return make(Set[T])
+func NewSet[K comparable]() Set[K] {
+	return make(Set[K])
 }
 
-func (set Set[T]) String() string {
+func (set Set[K]) Empty() bool {
+	return len(set) == 0
+}
+
+func (set Set[K]) Contains(k K) bool {
+	if _, ok := set[k]; ok {
+		return true
+	}
+
+	return false
+}
+
+func (set Set[K]) ContainsAll(ks []K) bool {
+	for _, k := range ks {
+		if !set.Contains(k) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (set Set[K]) Add(k K) {
+	set[k] = struct{}{}
+}
+
+func (set Set[K]) AddAll(ks []K) {
+	for _, k := range ks {
+		set.Add(k)
+	}
+}
+
+func (set Set[K]) Remove(k K) {
+	delete(set, k)
+}
+
+func (set Set[K]) Merge(s Set[K]) {
+	for e := range s {
+		set.Add(e)
+	}
+}
+
+func (set Set[K]) ToSlice() []K {
+	res := make([]K, 0, len(set))
+	for k := range set {
+		res = append(res, k)
+	}
+
+	return res
+}
+
+func FromSlice[K comparable](slc []K) Set[K] {
+	s := make(Set[K])
+	for _, e := range slc {
+		s.Add(e)
+	}
+	return s
+}
+
+func (set Set[K]) String() string {
 	strbuf := new(strings.Builder)
 	strbuf.WriteString("(")
 	for k := range set {
@@ -24,44 +84,77 @@ func (set Set[T]) String() string {
 	return strbuf.String()
 }
 
-func (set Set[T]) Contains(t T) bool {
-	if _, ok := set[t]; ok {
-		return true
-	}
-	return false
+type ConcurrentSet[K comparable] struct {
+	m  Set[K]
+	rw *sync.RWMutex
 }
 
-func (set Set[T]) Add(t T) {
-	set[t] = struct{}{}
-}
-
-func (set Set[T]) Del(t T) {
-	delete(set, t)
-}
-
-func (set Set[T]) AddList(ts []T) {
-	for _, t := range ts {
-		set.Add(t)
+func NewConcurrentSet[K comparable]() ConcurrentSet[K] {
+	return ConcurrentSet[K]{
+		m:  NewSet[K](),
+		rw: &sync.RWMutex{},
 	}
 }
 
-func (set Set[T]) Merge(o Set[T]) {
-	for e := range o {
-		set.Add(e)
-	}
+func (set *ConcurrentSet[K]) Empty() bool {
+	set.rw.RLock()
+	defer set.rw.RUnlock()
+
+	return set.m.Empty()
 }
 
-func (set Set[T]) ToSlice() []T {
-	res := make([]T, 0, len(set))
-	for k := range set {
-		res = append(res, k)
-	}
-	return res
+func (set *ConcurrentSet[K]) Contains(k K) bool {
+	set.rw.RLock()
+	defer set.rw.RUnlock()
+
+	return set.m.Contains(k)
 }
 
-func ToSet[T comparable](lst []T) Set[T] {
-	s := make(Set[T])
-	for _, e := range lst {
+func (set *ConcurrentSet[K]) ContainsAll(ks []K) bool {
+	set.rw.RLock()
+	defer set.rw.RUnlock()
+
+	return set.m.ContainsAll(ks)
+}
+
+func (set *ConcurrentSet[K]) Add(k K) {
+	set.rw.Lock()
+	defer set.rw.Unlock()
+
+	set.m.Add(k)
+}
+
+func (set *ConcurrentSet[K]) AddAll(ks []K) {
+	set.rw.Lock()
+	defer set.rw.Unlock()
+
+	set.m.AddAll(ks)
+}
+
+func (set *ConcurrentSet[K]) Remove(k K) {
+	set.rw.Lock()
+	defer set.rw.Unlock()
+
+	set.m.Remove(k)
+}
+
+func (set *ConcurrentSet[K]) Merge(s Set[K]) {
+	set.rw.Lock()
+	defer set.rw.Unlock()
+
+	set.m.Merge(s)
+}
+
+func (set *ConcurrentSet[K]) ToSlice() []K {
+	set.rw.RLock()
+	defer set.rw.RUnlock()
+
+	return set.m.ToSlice()
+}
+
+func ConcurrentFromSlice[K comparable](slc []K) ConcurrentSet[K] {
+	s := NewConcurrentSet[K]()
+	for _, e := range slc {
 		s.Add(e)
 	}
 	return s
--- set_test.go
+++ set_test.go
@@ -1,19 +1,101 @@
 package godskeqing_test
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	godskeqing "github.com/XuanLee-HEALER/gods-keqing"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSetMerge(t *testing.T) {
-	s1 := make(godskeqing.Set[int])
-	s2 := make(godskeqing.Set[int])
-	s1.AddList([]int{1, 2, 3})
-	s2.AddList([]int{2, 3, 4})
+func TestNewSet(t *testing.T) {
+	s := godskeqing.NewSet[int]()
+	assert.NotNil(t, s, "new set is nil")
+}
+
+func TestEmpty(t *testing.T) {
+	s := godskeqing.NewSet[int]()
+	assert.Equal(t, true, s.Empty(), "new set is not empty")
+}
+
+func TestAdd(t *testing.T) {
+	s := godskeqing.NewSet[int]()
+	s.Add(1)
+	assert.Contains(t, s, 1, "1 was added unsuccessfully")
+}
+
+func TestAddAll(t *testing.T) {
+	s := godskeqing.NewSet[int]()
+	s.AddAll([]int{1, 2, 3})
+	assert.Contains(t, s, 1, "1 was added unsuccessfully")
+	assert.Contains(t, s, 2, "2 was added unsuccessfully")
+	assert.Contains(t, s, 3, "3 was added unsuccessfully")
+}
+
+func TestContains(t *testing.T) {
+	s := godskeqing.NewSet[int]()
+	assert.Equal(t, false, s.Contains(1), "empty set contains 1")
+	s.Add(1)
+	assert.Equal(t, true, s.Contains(1), "1 was added unsuccessfully")
+}
+
+func TestContainsAll(t *testing.T) {
+	s := godskeqing.NewSet[int]()
+	assert.Equal(t, false, s.ContainsAll([]int{1, 2, 3}), "empty set contains [1,2,3]")
+	s.AddAll([]int{1, 2, 3})
+	assert.Equal(t, true, s.ContainsAll([]int{1, 2, 3}), "[1,2,3] was added unsuccessfully")
+}
+
+func TestRemove(t *testing.T) {
+	s := godskeqing.NewSet[int]()
+	s.AddAll([]int{1, 2, 3})
+	s.Remove(1)
+	assert.Equal(t, true, s.ContainsAll([]int{2, 3}), "1 was deleted unsuccessfully")
+}
+
+func TestMerge(t *testing.T) {
+	s1 := godskeqing.NewSet[int]()
+	s2 := godskeqing.NewSet[int]()
+	s1.AddAll([]int{1, 2, 2, 3})
+	s2.AddAll([]int{2, 3, 4, 5})
 	s1.Merge(s2)
-	if len(s1) != 4 {
-		t.Fail()
+	assert.ElementsMatch(t, s1.ToSlice(), []int{1, 2, 3, 4, 5}, "failed to merge")
+}
+
+func TestToSlice(t *testing.T) {
+	s := godskeqing.NewSet[int]()
+	s.AddAll([]int{1, 2, 3})
+	l := s.ToSlice()
+	assert.ElementsMatch(t, []int{1, 2, 3}, l, "set converted to slice has changed")
+	l = []int{}
+	assert.ElementsMatch(t, s.ToSlice(), []int{1, 2, 3}, "set changed by new reference")
+	assert.ElementsMatch(t, l, []int{}, "")
+}
+
+func TestFromSlice(t *testing.T) {
+	arr := []int{1, 2, 2, 3, 4, 5, 5, 6}
+	s := godskeqing.FromSlice[int](arr)
+	assert.ElementsMatch(t, s.ToSlice(), []int{1, 2, 3, 4, 5, 6}, "set converted by slice is wrong")
+}
+
+func TestConcurrentSet(t *testing.T) {
+	s := godskeqing.NewConcurrentSet[int]()
+
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			time.Sleep(2 * time.Second)
+			for xi := 1; xi <= 5; xi++ {
+				if xi*i/2 == 0 {
+					s.Add(xi)
+				}
+			}
+		}(i)
 	}
-	t.Logf("merge result: %v", s1)
+	wg.Wait()
+
+	assert.ElementsMatch(t, s.ToSlice(), []int{1, 2, 3, 4, 5}, "wrong values")
 }
